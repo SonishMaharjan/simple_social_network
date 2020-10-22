77ccd2f36ef12b33ae790b80be5f78f1ae2de945
sonish <sonishmzn95@gmail.com>
2020-10-22 15:16:05 +0545

    consumer created

---
+++ social_network/chat/consumers.py
@@ -0,0 +1,21 @@
+# it's like a view for normal request,
+#it accepts the web socket request and process it
+
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        self.send(text_data = json.dumps({
+            'message': message
+        }))
--- social_network/social_network/routing.py
+++ social_network/social_network/routing.py
@@ -1,5 +1,14 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
+# ProtocolTypeRouter will first inspect the type of connection.
+# If it is a WebSocket connection (ws:// or wss://),
+# the connection will be given to the AuthMiddlewareStack.
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    )
+
 })
